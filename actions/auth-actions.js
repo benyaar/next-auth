cb176a3dfc7cf3d20a96a5eacdac928d897c45e7
Artur <82472015+benyaar@users.noreply.github.com>
2025-06-05 00:05:24 +0300

    Checking for Email Duplication

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -2,22 +2,33 @@
 
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
 
-export async function signup(prevState, formData){
+export async function signup(prevState, formData) {
     const email = formData.get('email')
     const password = formData.get('password')
     let errors = {}
-    if(!email.includes('@')){
+    if (!email.includes('@')) {
         errors.email = 'No valid email'
     }
-    if(password.trim().length < 8){
+    if (password.trim().length < 8) {
         errors.password = 'No valid password'
     }
-    if(Object.keys(formState.errors).length > 0 ){
+    if (Object.keys(formState.errors).length > 0) {
         return {
             errors
         }
     }
-    createUser(email, hashUserPassword(password))
-    
+    try {
+        createUser(email, hashUserPassword(password))
+    } catch (error) {
+        if(error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+            return {errors: {
+                email: 'email already exists'
+            }}
+        }
+        throw error;
+    }
+    redirect('/training');
+
 } 
